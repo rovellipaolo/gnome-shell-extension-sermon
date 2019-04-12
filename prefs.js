a36f03a0c97bdf39f5045c242da9570da5036efa
Paolo Rovelli <rovellipaolo@gmail.com>
2019-04-12 17:59:09 +0200

    Add Settings widget

---
+++ prefs.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+const Convenience = Me.imports.src.util.convenience;
+
+/** Triggered after the file is loaded but before the buildPrefsWidget. */
+/* exported init */
+const init = () => {
+}
+
+/** Triggered when opening the extension preferences widget. */
+/* exported buildPrefsWidget */
+const buildPrefsWidget = () => {
+    const builder = _getWidgetBuilder();
+    _bindWidgetToSettings(builder);
+
+    const widget = _buildWidget(builder);
+    widget.show_all();
+
+    return widget;
+};
+
+const _getWidgetBuilder = () => {
+    const builder = new Gtk.Builder();
+    builder.add_from_file(`${Me.dir.get_path()}/prefs.xml`);
+    return builder;
+}
+
+const _bindWidgetToSettings = (builder) => {
+    const settings = Convenience.getSettings();
+    settings.bind("max-items-per-section", builder.get_object("field_max_items_per_section"), "value", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind("systemd-section-enabled", builder.get_object("field_systemd_section_enabled"), "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind("systemd-section-items-priority-list", builder.get_object("field_systemd_section_items_priority_list"), "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind("docker-section-enabled", builder.get_object("field_docker_section_enabled"), "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind("docker-section-items-priority-list", builder.get_object("field_docker_section_items_priority_list"), "text", Gio.SettingsBindFlags.DEFAULT);
+}
+
+const _buildWidget = (builder) => {
+    return builder.get_object("vbox_built");
+}
+
