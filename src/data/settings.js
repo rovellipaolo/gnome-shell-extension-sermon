dadf530e8330e4d269837b8a8d5c4a0103e6e06a
Paolo Rovelli <rovellipaolo@gmail.com>
2022-05-20 23:10:45 +0200

    Move GSettings initialization outside global scope

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,31 +1,44 @@
 "use strict";
 
-const GSettings = imports.misc.extensionUtils.getSettings();
+const ExtensionUtils = imports.misc.extensionUtils;
 
 const ITEMS_PRIORITY_LIST_SEPARATOR = ",";
 
 /* exported getMaxItemsPerSection */
-var getMaxItemsPerSection = () => GSettings.get_int("max-items-per-section");
+var getMaxItemsPerSection = () =>
+    ExtensionUtils.getSettings().get_int("max-items-per-section");
 
 /* exported bindMaxItemsPerSection */
 var bindMaxItemsPerSection = (field, flags) =>
-    GSettings.bind("max-items-per-section", field, "value", flags);
+    ExtensionUtils.getSettings().bind(
+        "max-items-per-section",
+        field,
+        "value",
+        flags
+    );
 
 /* exported isSystemdSectionEnabled */
 var isSystemdSectionEnabled = () =>
-    GSettings.get_boolean("systemd-section-enabled");
+    ExtensionUtils.getSettings().get_boolean("systemd-section-enabled");
 
 /* exported bindSystemdSectionEnabled */
 var bindSystemdSectionEnabled = (field, flags) =>
-    GSettings.bind("systemd-section-enabled", field, "active", flags);
+    ExtensionUtils.getSettings().bind(
+        "systemd-section-enabled",
+        field,
+        "active",
+        flags
+    );
 
 /* exported shouldFilterSystemdLoadedServices */
 var shouldFilterSystemdLoadedServices = () =>
-    GSettings.get_boolean("systemd-section-filter-loaded-services");
+    ExtensionUtils.getSettings().get_boolean(
+        "systemd-section-filter-loaded-services"
+    );
 
 /* exported bindFilterSystemdLoadedServices */
 var bindFilterSystemdLoadedServices = (field, flags) =>
-    GSettings.bind(
+    ExtensionUtils.getSettings().bind(
         "systemd-section-filter-loaded-services",
         field,
         "active",
@@ -34,11 +47,13 @@ var bindFilterSystemdLoadedServices = (field, flags) =>
 
 /* exported shouldFilterSystemdUserServices */
 var shouldFilterSystemdUserServices = () =>
-    GSettings.get_boolean("systemd-section-filter-user-services");
+    ExtensionUtils.getSettings().get_boolean(
+        "systemd-section-filter-user-services"
+    );
 
 /* exported bindFilterSystemdUserServices */
 var bindFilterSystemdUserServices = (field, flags) =>
-    GSettings.bind(
+    ExtensionUtils.getSettings().bind(
         "systemd-section-filter-user-services",
         field,
         "active",
@@ -47,11 +62,13 @@ var bindFilterSystemdUserServices = (field, flags) =>
 
 /* exported shouldFilterSystemdServicesByPriorityList */
 var shouldFilterSystemdServicesByPriorityList = () =>
-    GSettings.get_boolean("systemd-section-filter-priority-list");
+    ExtensionUtils.getSettings().get_boolean(
+        "systemd-section-filter-priority-list"
+    );
 
 /* exported bindFilterSystemdServicesByPriorityList */
 var bindFilterSystemdServicesByPriorityList = (field, flags) =>
-    GSettings.bind(
+    ExtensionUtils.getSettings().bind(
         "systemd-section-filter-priority-list",
         field,
         "active",
@@ -61,35 +78,58 @@ var bindFilterSystemdServicesByPriorityList = (field, flags) =>
 /* exported getSystemdSectionItemsPriorityList */
 var getSystemdSectionItemsPriorityList = () =>
     mapCommaSeparatedListIntoArray(
-        GSettings.get_string("systemd-section-items-priority-list")
+        ExtensionUtils.getSettings().get_string(
+            "systemd-section-items-priority-list"
+        )
     );
 
 /* exported bindSystemdSectionItemsPriorityList */
 var bindSystemdSectionItemsPriorityList = (field, flags) =>
-    GSettings.bind("systemd-section-items-priority-list", field, "text", flags);
+    ExtensionUtils.getSettings().bind(
+        "systemd-section-items-priority-list",
+        field,
+        "text",
+        flags
+    );
 
 /* exported isCronSectionEnabled */
-var isCronSectionEnabled = () => GSettings.get_boolean("cron-section-enabled");
+var isCronSectionEnabled = () =>
+    ExtensionUtils.getSettings().get_boolean("cron-section-enabled");
 
 /* exported bindCronSectionEnabled */
 var bindCronSectionEnabled = (field, flags) =>
-    GSettings.bind("cron-section-enabled", field, "active", flags);
+    ExtensionUtils.getSettings().bind(
+        "cron-section-enabled",
+        field,
+        "active",
+        flags
+    );
 
 /* exported isDockerSectionEnabled */
 var isDockerSectionEnabled = () =>
-    GSettings.get_boolean("docker-section-enabled");
+    ExtensionUtils.getSettings().get_boolean("docker-section-enabled");
 
 /* exported bindDockerSectionEnabled */
 var bindDockerSectionEnabled = (field, flags) =>
-    GSettings.bind("docker-section-enabled", field, "active", flags);
+    ExtensionUtils.getSettings().bind(
+        "docker-section-enabled",
+        field,
+        "active",
+        flags
+    );
 
 /* exported isPodmanSectionEnabled */
 var isPodmanSectionEnabled = () =>
-    GSettings.get_boolean("podman-section-enabled");
+    ExtensionUtils.getSettings().get_boolean("podman-section-enabled");
 
 /* exported bindPodmanSectionEnabled */
 var bindPodmanSectionEnabled = (field, flags) =>
-    GSettings.bind("podman-section-enabled", field, "active", flags);
+    ExtensionUtils.getSettings().bind(
+        "podman-section-enabled",
+        field,
+        "active",
+        flags
+    );
 
 var mapCommaSeparatedListIntoArray = (str) =>
     str
